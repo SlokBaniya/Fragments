aaf78b7a6a8b1875772d02ad8d0f3a035607c59c
Softwarica College Lab <softwarica@softwarica.com>
2019-04-21 11:27:34 +0545

    Fragment Created.. Adding numbers and area of circle

--- app/src/main/java/com/e/fragments/MainActivity.java
+++ app/src/main/java/com/e/fragments/MainActivity.java
@@ -1,13 +1,56 @@
 package com.e.fragments;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import fragments.FirstFragment;
+import fragments.SecondFragment;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnFragment;
+    private Boolean Status = true;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnFragment = findViewById(R.id.btnFrag);
+        btnFragment.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        FragmentManager fragmentManager =getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (Status){
+            FirstFragment firstFragment = new FirstFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer,firstFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            btnFragment.setText("Load Second Fragment");
+            Status = false;
+
+        } else{
+            SecondFragment secondFragment = new SecondFragment();
+            fragmentTransaction.replace(R.id.fragmentContainer,secondFragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+            btnFragment.setText("Load FirstFragment");
+            Status = true
+            ;
+
+        }
     }
 }
---
+++ app/src/main/java/fragments/FirstFragment.java
@@ -0,0 +1,54 @@
+package fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.e.fragments.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment implements View.OnClickListener{
+    private Button btnCalculate;
+    private EditText etFirst, etSecond;
+
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+
+        View view = inflater.inflate(R.layout.fragment_first,container,false);
+
+        etFirst = view.findViewById(R.id.etFirst);
+        etSecond = view.findViewById(R.id.etSecond);
+        btnCalculate = view.findViewById(R.id.btnCalculate);
+
+        btnCalculate.setOnClickListener(this);
+        return view;
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int First = Integer.parseInt(etFirst.getText().toString());
+        int Second = Integer.parseInt(etSecond.getText().toString());
+        int result = First + Second;
+
+        Toast.makeText(getActivity(),"Sum is "+result, Toast.LENGTH_SHORT).show();
+
+    }
+}
